3b8cf7d0f7dd22031efae0dffa7576c8554751d4
Joakim Sehman <sehman@student.chalmers.se>
2015-09-29 21:11:53 +0200

    created ChatActivity, coding commencing

--- Discbuss/app/src/main/java/teamosqar/application/ChatController.java
+++ Discbuss/app/src/main/java/teamosqar/application/ChatController.java
@@ -1,7 +1,14 @@
 package teamosqar.application;
 
+import com.firebase.client.Firebase;
+
 /**
  * Created by joakim on 2015-09-29.
  */
 public class ChatController {
+
+    private String myUsername;
+    private Firebase chatFireBaseRef;
+
+    
 }
