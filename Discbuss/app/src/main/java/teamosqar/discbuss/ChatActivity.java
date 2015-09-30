9a4e74ec3dd27ae538532035f546d2a96bd6737b
Joakim Sehman <sehman@student.chalmers.se>
2015-09-30 10:41:06 +0200

    chat code initiated

--- Discbuss/app/src/main/java/teamosqar/discbuss/ChatActivity.java
+++ Discbuss/app/src/main/java/teamosqar/discbuss/ChatActivity.java
@@ -1,8 +1,29 @@
 package teamosqar.discbuss;
 
+import android.app.ListActivity;
+import android.os.Bundle;
+
+import teamosqar.application.ChatController;
+
 /**
  * Created by joakim on 2015-09-29.
  */
-public class ChatActivity {
+public class ChatActivity extends ListActivity {
 
+    private ChatController chatController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        chatController = new ChatController();
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+    public void onStop(){
+        super.onStop();
+    }
 }
--- Discbuss/app/src/main/java/teamosqar/util/Message.java
+++ Discbuss/app/src/main/java/teamosqar/util/Message.java
@@ -4,4 +4,40 @@ package teamosqar.util;
  * Created by joakim on 2015-09-29.
  */
 public class Message {
+
+    private String message;
+    private String author;
+    private int karma;
+
+    //required default constructor for firebase object mapping
+    private Message(){
+    }
+
+    public Message(String message, String author){
+        this.message = message;
+        this.author = author;
+        karma = 0;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public int getKarma(){
+        return karma;
+    }
+
+
+    //sync with firebase??
+    public void upVote(){
+        karma++;
+    }
+
+    public void downVote(){
+        karma--;
+    }
 }
