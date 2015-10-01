5fc36bb702285402e7ef874b8df9c972c626ec1f
Joakim Sehman <sehman@student.chalmers.se>
2015-10-01 16:17:23 +0200

    chatController code

--- Discbuss/app/src/main/java/teamosqar/discbuss/application/ChatController.java
+++ Discbuss/app/src/main/java/teamosqar/discbuss/application/ChatController.java
@@ -1,5 +1,7 @@
 package teamosqar.discbuss.application;
 
+import android.util.Log;
+
 import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
@@ -34,38 +36,81 @@ public class ChatController extends Observable {
         chatListener = chatFireBaseRef.addChildEventListener(new ChildEventListener() {
 
             @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildKey) {
 
                 Message message = dataSnapshot.getValue(Message.class);
                 String key = dataSnapshot.getKey();
 
                 //Insert into correct location, based on previous child
-                if(previousChildName == null){
+                if(previousChildKey == null){
                     messageModels.add(0, message);
                     messageKeys.add(0,key);
                 }else{
-                    int previousIndex = messageKeys.indexOf(previousChildName);
+                    int previousIndex = messageKeys.indexOf(previousChildKey);
+                    int nextIndex = previousIndex + 1;
+                    if(nextIndex == messageModels.size()){
+                        messageModels.add(message);
+                        messageKeys.add(key);
+                    }else{
+                        messageModels.add(nextIndex, message);
+                        messageKeys.add(key);
+                    }
                 }
+
+                notifyObservers();
             }
 
             @Override
             public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                String key = dataSnapshot.getKey();
+                Message message = dataSnapshot.getValue(Message.class);
+                int index = messageKeys.indexOf(key);
 
+                messageModels.set(index, message);
+
+                notifyObservers();
             }
 
             @Override
             public void onChildRemoved(DataSnapshot dataSnapshot) {
+                String key = dataSnapshot.getKey();
+                int index = messageKeys.indexOf(key);
 
+                messageKeys.remove(index);
+                messageModels.remove(index);
+
+                notifyObservers();
             }
 
             @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName) {
+            public void onChildMoved(DataSnapshot dataSnapshot, String previousChildKey) {
+                String key = dataSnapshot.getKey();
+                Message message = dataSnapshot.getValue(Message.class);
 
+                int index = messageKeys.indexOf(key);
+                messageModels.remove(index);
+                messageKeys.remove(index);
+
+                if(previousChildKey == null){
+                    messageModels.add(0, message);
+                    messageKeys.add(0, key);
+                }else{
+                    int previousIndex = messageKeys.indexOf(previousChildKey);
+                    int nextIndex = previousIndex + 1;
+                    if(nextIndex == messageModels.size()){
+                        messageModels.add(message);
+                        messageKeys.add(key);
+                    }else{
+                        messageModels.add(nextIndex, message);
+                        messageKeys.add(nextIndex, key);
+                    }
+                }
+                notifyObservers();
             }
 
             @Override
             public void onCancelled(FirebaseError firebaseError) {
-
+                Log.e("FirebaseListAdapter", "Listen was cancelled, no more updates will occur");
             }
         });
     }
