27324aefa938dc13060ae1f1493cd24c11e95e6a
Joakim Sehman <sehman@student.chalmers.se>
2015-10-02 17:59:46 +0200

    LoginActivity, LoginController created

--- Discbuss/app/src/main/java/teamosqar/discbuss/activities/LoginActivity.java
+++ Discbuss/app/src/main/java/teamosqar/discbuss/activities/LoginActivity.java
@@ -4,28 +4,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.EditText;
 
-import com.firebase.client.Firebase;
+import teamosqar.discbuss.activities.R;
+import teamosqar.discbuss.application.LoginController;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
+
+    private LoginController loginController;
+    private EditText editName, editPassword;
+    private String name, password;
+    private Button buttonLogin;
 
     @Override
     protected void onStart(){
         super.onStart();
-        Firebase.setAndroidContext(this);
-
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        editName = (EditText)findViewById(R.id.editTextName);
+        editPassword = (EditText)findViewById(R.id.editTextPassword);
+        buttonLogin = (Button)findViewById(R.id.buttonLogin);
+        loginController = new LoginController();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_login, menu);
         return true;
     }
 
---
+++ Discbuss/app/src/main/java/teamosqar/discbuss/application/LoginController.java
@@ -0,0 +1,9 @@
+package teamosqar.discbuss.application;
+
+/**
+ * Created by joakim on 2015-10-02.
+ */
+public class LoginController {
+
+
+}
