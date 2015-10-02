46cb55cc79effa47783ad3d6838a3b1ebdac76a6
Jakob Holmgren <jholmgren95@gmail.com>
2015-10-02 17:25:19 +0200

    Basic registration with toasts

--- Discbuss/app/src/main/java/teamosqar/discbuss/activities/MainActivity.java
+++ Discbuss/app/src/main/java/teamosqar/discbuss/activities/MainActivity.java
@@ -1,9 +1,11 @@
 package teamosqar.discbuss.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 
 import com.firebase.client.Firebase;
@@ -29,7 +31,10 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
+    public void goToReg(View view){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
