ff1799dcc4387f2d9447e103a8c29d46f63a5270
Joakim Sehman <sehman@student.chalmers.se>
2015-10-07 19:45:55 +0200

    added files for previous commit

---
+++ Discbuss/app/src/main/java/teamosqar/discbuss/fragments/LoadingFragment.java
@@ -0,0 +1,26 @@
+package teamosqar.discbuss.fragments;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import teamosqar.discbuss.activities.R;
+
+/**
+ * Created by joakim on 2015-10-07.
+ */
+public class LoadingFragment extends DialogFragment {
+
+    public LoadingFragment(){
+
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_loading, container, false);
+    }
+
+}
