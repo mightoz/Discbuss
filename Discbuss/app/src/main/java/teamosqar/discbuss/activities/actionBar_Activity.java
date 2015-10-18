b1feb4eb0da78f1b7403cf8560fa66215ebf2362
Oskar Rutqvist <rutanjr@gmail.com>
2015-10-18 19:59:53 +0200

    actionbar layout improved

--- Discbuss/app/src/main/java/teamosqar/discbuss/activities/actionBar_Activity.java
+++ Discbuss/app/src/main/java/teamosqar/discbuss/activities/actionBar_Activity.java
@@ -1,14 +1,16 @@
 package teamosqar.discbuss.activities;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 
-public class ActionBar_Activity extends ActionBarActivity {
+public class ActionBar_Activity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action_bar);
     }
+
     @Override
     protected void onResume() {
         // TODO Auto-generated method stub
