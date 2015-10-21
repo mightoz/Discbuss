a49c7cfda1409fd6eba155ebef4cd2315af5fa1e
Mightoz <oscar.beronius@gmail.com>
2015-10-21 10:33:39 +0200

    Formatted bus stop for simulated bus trip.

--- Discbuss/app/src/main/java/teamosqar/discbuss/application/Model.java
+++ Discbuss/app/src/main/java/teamosqar/discbuss/application/Model.java
@@ -154,7 +154,24 @@ public class Model extends Observable implements Observer{
 
     @Override
     public void update(Observable observable, Object data) {
-        nextBusStop = stopUpdater.getNextBusStop();
+        String busStopTmp = stopUpdater.getNextBusStop();
+        switch (busStopTmp){
+            case "G�taplatsen":
+                nextBusStop = "Götaplatsen";
+                break;
+            case "Kungsportsplatsn":
+                nextBusStop = "Kungsportsplatsen";
+                break;
+            case "NisseTerminalen":
+                nextBusStop = "Nils Ericson Terminalen";
+                break;
+            case "Frihamnen":
+                nextBusStop = busStopTmp;
+                break;
+            case "Lindholmen":
+                nextBusStop = busStopTmp;
+                break;
+        }
         setChanged();
         notifyObservers();
     }
