6feb53c0f185a33c0f6ddcd9f0a6c9cda95d7d6c
Jakob Holmgren <jholmgren95@gmail.com>
2015-10-29 12:05:38 +0100

    Message commented

--- Discbuss/app/src/main/java/teamosqar/discbuss/util/Message.java
+++ Discbuss/app/src/main/java/teamosqar/discbuss/util/Message.java
@@ -10,6 +10,11 @@ public class Message implements Comparable<Message> {
     private String author;
     private int karma;
 
+    /**
+     * Compares the message calling the method to the method provided as param based on karma
+     * @param other
+     * @return 1 if the message calling the method is superior to the message provided as param in terms of current karma, 0 if equal, -1 if less.
+     */
     public int compareTo(Message other){
         int result = 0;
         result = Integer.compare(karma, other.getKarma());
@@ -34,20 +39,36 @@ public class Message implements Comparable<Message> {
         this.karma = karma;
     }
 
+    /**
+     * @return the UID of the author of the message
+     */
     public String getUid(){ return uid; }
 
+    /**
+     * @return the message itself
+     */
     public String getMessage(){
         return message;
     }
 
+    /**
+     * @return the author of the message
+     */
     public String getAuthor(){
         return author;
     }
 
+    /**
+     * @return the karma of the message
+     */
     public int getKarma(){
         return karma;
     }
 
+    /**
+     * Sets the karma of the message based on the input param
+     * @param karma
+     */
     public void setKarma(int karma){
         this.karma = karma;
     }
